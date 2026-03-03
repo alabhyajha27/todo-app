037280824eb23d56bfcabf15d05dcef15c3e4c01
Alabhya Jha <alabhyajha7@gmail.com>
2026-03-03 10:04:12 +0530

    Modify app run configuration for port and host
    Updated app to run on specified port from environment variable.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
@@ -38,8 +39,9 @@ def delete(todo_id):
     db.session.commit()
     return redirect(url_for("index"))
 
-if __name__=='__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
